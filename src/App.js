de7e31243c07dc39ee2ee7bcf9a741df1ccc5995
Maria Eduarda Borges <duda.pborges92@gmail.com>
2023-02-09 17:00:12 -0300

    randomly generate cards

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import capa from './assets/capa.png'
+import cover from './assets/cover.png'
+
+const cardImages = [
+  {"src": "/assets/monse-1.png"},
+  {"src": "/assets/cesar-1.png"},
+  {"src": "/assets/jamal-1.png"},
+  {"src": "/assets/ruby-1.png"},
+  {"src": "/assets/jasmin-1.png"},
+  {"src": "/assets/own-my-block-1.png"}
+
+]
 
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [showCapa, setShowCapa] = useState(true)
+
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({ ...card, id: Math.random() }))
+
+    setCards(shuffleCards)
+    setTurns(0)
+    setShowCapa(false)
+  }
+
+  console.log(cards, turns)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Own My Block Memory Game</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      {showCapa && <div className='capa'>
+      <img className='img-capa' src={capa}></img>
+      </div>}
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <div className='card' key={card.id}>
+            <img className='front' src={card.src}/>
+            <img className='back' src={cover}/>
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
